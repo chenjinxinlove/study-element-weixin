4a3bf5514229acdf750a03b39a62225a80a7471f
陈金鑫 <505150704@qq.com>
2017-12-01 20:36:08 +0800

    添加聊天信息

--- src/js/pages/Home/Chats/index.js
+++ src/js/pages/Home/Chats/index.js
@@ -1,19 +1,61 @@
 import React, {Component} from 'react';
 import { inject, observer} from 'mobx-react';
 import clazz from 'classname';
+import moment from 'moment';
 
 import classes from './style.css';
 import Avatar from 'components/Avatar';
+
+moment.updateLocale('en', {
+    relativeTime: {
+        past: '%s',
+        m: 'a min',
+        mm: '%d mins',
+        h: 'an h',
+        hh: '%d h',
+        s: 'now',
+        ss: '%d s'
+    }
+})
 
 @inject(stores => ({
     chats: stores.home.chats,
     loading: stores.session.loading,
     selected: stores.home.user,
+    messages: stores.home.messages,
     chatTo: stores.home.chatTo
 }))
 
 @observer 
 export default class Chats extends Component {
+
+    getTheLastestMessage(userid) {
+        let list = this.props.messages[userid] || [];
+
+        let res;
+
+        list.map(e => {
+            if (e.FromUserName === userid) {
+                res = e;
+            }
+        })
+    }
+
+    getMessageContent(message) {
+        switch (message.MsgType) {
+            case 1: 
+                return message.Content;
+
+            case 3:
+                return '[Image]';
+        }
+    }
+
+    hasUnreadMessage(userid) {
+        let list = this.props.messages[userid] || [];
+        return list.length != (list.unread || 0);
+    }
+
     render () {
         let { loading, chats , selected, chatTo} = this.props;
         if(loading) return false;
@@ -23,18 +65,27 @@ export default class Chats extends Component {
                 <div className={classes.chats}>
                     {
                         chats.map((e, index) => {
+                            let message = this.getTheLastestMessage(e.UserName) || {};
+                            
                             return (
                                 <div className={clazz(classes.chat, selected && selected.UserName === e.UserName && classes.active)} key={index} onClick={ev => chatTo(e)}>
                                     <div className={classes.inner}>
-                                    <Avatar src={e.HeadImgUrl}/>
-                                        <div className={classes.info}>
-                                            <p className={classes.username} dangerouslySetInnerHTML={{__html: e.RemarkName || e.NickName}}/>
+                                    <div className={this.hasUnreadMessage(e.UserName) && classes.reddot}>
+                                        <Avatar src={e.HeadImgUrl}/>
+                                    </div>
+                                    
+                                    <div className={classes.info}>
+                                        <p className={classes.username} dangerouslySetInnerHTML={{__html: e.RemarkName || e.NickName}}/>
 
-                                            <span className={classes.message} dangerouslySetInnerHTML={{__html: e.PYQuanPin || '没有信息'}}/>
-                                        </div>
+                                        <span className={classes.message} dangerouslySetInnerHTML={{__html: this.getMessageContent(message) || '没有信息'}}/>
+                                    </div>
                                     </div>
 
-                                    <span className={classes.times}>11 mins</span>    
+                                    <span className={classes.times}>
+                                        {
+                                            message.CreateTime ? moment(message.CreateTime * 1000).fromNow() : ''
+                                        }
+                                    </span>    
                                 </div>    
                             );
                         })
--- src/js/stores/home.js
+++ src/js/stores/home.js
@@ -1,16 +1,40 @@
 
 import { observable, action } from 'mobx';
+import axios from 'axios';
 
+import storage from 'utils/storage';
 import contacts from './contacts';
+import session from './session';
 
 function unique(arr) {
     return [... new Set([...arr])]
 }
 
+async function resolveMessage(message) {
+    var auth = await storage.get('auth');
+
+    switch (message.MsgType) {
+        case 3:
+            let string = message.Content.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+            let matchs = string.match(/(\w+)="([^\s]+)"/g);
+            let images = {};
+
+            matchs.map(e => {
+                var kv = e.replace(/"/g, '').split('=');
+
+                images[kv[0]] = kv[1];
+            });
+
+            images.src = `${axios.defaults.baseURL}/cgi-bin/mmwebwx-bin/webwxgetmsgimg?&MsgID=${message.MsgId}&skey=${auth.skey}`.replace(/\/+/g, '/');
+            message.images = images;
+            return message;
+    }
+}
+
 class Home {
     @observable chats = [];
-
-    @observable user;
+    @observable messages = []
+    @observable user = false;
 
     users;
 
@@ -19,9 +43,9 @@ class Home {
             return self.users;
         }
 
-        self.user = await contacts.getList();
+        self.users = await contacts.getList();
 
-        return self.user;
+        return self.users;
     }
 
     @action async loadChats(chatSet) {
@@ -37,11 +61,98 @@ class Home {
         });
 
         self.chats.replace(res);
+
+        res.map(e => {
+            self.messages[e.UserName] = [];
+        })
         return res;
     }
 
     @action chatTo(user) {
         self.user = user;
+        self.markedRead(user.UserName);
+    }
+
+    @action async addMessage(message) {
+        let from = message.FromUserName;
+        let messages = Object.assign({}, self.messages);
+        let list = messages[from];
+
+        if (list) {
+            if(!list.find(e => e.NewMsgId === message.NewMsgId)) {
+                message = await resolveMessage(message);
+                list.push(message);
+            } else {
+                let user = self.users[from];
+
+                if (user) {
+                    self.chats.shift(user);
+                    list = messages[from] = [message];
+                }
+            }
+        }
+
+        if (self.user.UserName === from ) {
+            list.unread = list.length;
+        } else {
+            list.unread = 0;
+        }
+
+        self.messages = messages;
+    }
+
+    @action async sendMessage(content) {
+        let id = (+new Date() * 1000) + Math.random().toString().substr(2, 4);
+        let auth = await storage.get('auth');
+        let from = session.user.User.UserName;
+        let to = self.user.UserName;
+        let response = await axios.post(`/cgi-bin/mmwebwx-bin/webwxsendmsg`, {
+            BaseRequest: {
+                Sid: auth.wxsid,
+                Uin: auth.wxuin,
+                Skey: auth.skey
+            },
+            Msg: {
+                Content: content,
+                FromUserName: from,
+                ToUserName: to,
+                ClientMsgId: id,
+                LocalID: id,
+                Type: 1
+            },
+            Scene: 0
+        });
+
+        if (+response.data.BaseResponse.Ret === 0) {
+            let messages = Object.assign({}, self.messages);
+
+            messages[to].push({
+                isme: true,
+                Content: content,
+                MsgType: 1,
+                CreateTime: +new Date() / 1000,
+                HeadImgUrl: `${axios.defaults.baseURL}${session.user.User.HeadImgUrl}`.replace(/\/+/g, '/')
+            });
+
+            self.markedRead(to);
+        } else {
+            console.error('Failed to send message:', response.data);
+        }
+
+        return +response.data.BaseResponse.Ret === 0;
+    }
+
+    @action markedRead(userid) {
+        var messages = Object.assign({}, self.messages);
+        var list = messages[userid];
+
+        if (list) {
+            list.unread = list.length;
+            self.messages = messages;
+        } else {
+            // Init the message queue
+            self.messages[userid] = [];
+        }
     }
 }
 
